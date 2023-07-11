f2b3bc79513b7bb632df262f7c6253f7a4b59cb1
Gome510 <bluehawkfire55@gmail.com>
2023-07-11 14:53:53 -0700

    Background Not Rendering

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <img src="./assets/table.jpg"/>
+    </div>
+  )
+}
+
+export default App
