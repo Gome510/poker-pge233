c47a66106dbb5a7e4cfc2aa52e827334daa8348c
Gome510 <bluehawkfire55@gmail.com>
2023-07-15 18:36:30 -0700

    Background fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
+import backgroundImage from './assets/table.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <img src="./assets/table.jpg"/>
+    <div className="background-image">
+      {/* Your component's content */}
     </div>
   )
 }
