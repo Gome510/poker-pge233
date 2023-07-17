3e3ce14fbf30ccd3aeaa9f5c8ce7ea000ea8f77b
Gome510 <bluehawkfire55@gmail.com>
2023-07-17 15:52:03 -0700

    action buttons

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -5,7 +5,9 @@ export default function ActionBar() {
     return (
         <div className="action-bar">
             <button className="action-button">Button 1</button>
-
+            <button className="action-button">Button 2</button>
+            <button className="action-button">Button 3</button>
+            //input type range
         </div>
     )
 }
