ed775c603e5765e461f1f2d424c3ac113a321f4d
Gome510 <bluehawkfire55@gmail.com>
2023-12-22 22:59:10 -0400

    [test] add AnteInAllPlayers CpuAction tests

--- src/test.js
+++ src/test.js
@@ -1,10 +1,47 @@
-import { Deck } from "./components/Deck.js";
+import { Poker } from "./components/Poker.js";
 
-let deck = new Deck();
+testAnteInAllPlayers();
+testCpuAction();
 
-deck.shuffle();
-deck.draw(2);
-console.log(await deck.exists());
+function testAnteInAllPlayers() {
+  let pass = true;
+  const game = new Poker();
+  for (let i = 0; i < game.players.length; i++) {
+    if (game.currentPlayer().isCPU) {
+      game.cpuAction();
+    } else {
+      game.action("ante-in");
+    }
+  }
 
-let nonExistantDeck = new Deck({ id: "asdfjsoif" });
-console.log(await nonExistantDeck.exists());
+  for (let i = 0; i < game.players.length; i++) {
+    const player = game.players[i];
+    if (player.balance != 9990) {
+      console.log(
+        `testAnteInAllPlayers: Error: Player ${player.seat}'s balance ${player.balance} after ante-in`
+      );
+      pass = false;
+    }
+  }
+
+  console.log(`testAnteInAllPlayers: ${pass ? "Success" : "Failed"}`);
+  return;
+}
+
+function testCpuAction(initialState = {}) {
+  let game = new Poker(initialState);
+  let pass = true;
+  game.cpuAction();
+
+  if (game.playerTurn != 1) {
+    console.log(`testCpuAction: Error: playerTurn ${game.playerTurn}`);
+    pass = false;
+  }
+
+  if (game.players[0].balance != 10000 - game.ante) {
+    console.log(`testCpuAction: Error: player 1 balance ${game.playerTurn}`);
+    pass = false;
+  }
+
+  console.log(`testCpuAction: ${pass ? "Success" : "Failed"}`);
+}
